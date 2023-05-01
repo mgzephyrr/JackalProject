49a28c34eca388c87b47bbb72f75e8448f84dd65
Алиса Долныкова <akdolnykova@edu.hse.ru>
2023-05-01 18:01:09 +0500

    Камера двигается по клавишам W, A, S, D, также по стрелочкам. Также работает Shift для ускорения.

--- Assets/Scripts/CameraControled.cs
+++ Assets/Scripts/CameraControled.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class CameraControled : MonoBehaviour
 {
     public float rotateSpeed = 10.0f; //скорость, с которой вращается камера
+    public float speed = 10.0f; //скоростб движения камеры
 
-    private float mult = 1f;
+    private float mult = 1f; //ускорение
 
     private void Update()
     {
+        float horizont = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         float rotate = 0f;
         if (Input.GetKey(KeyCode.Q)) //если нажата клавиша Q, то камера вращается по часовой стрелке
             rotate = -1f;
         else if (Input.GetKey(KeyCode.E)) //если нажата клавиша Q, то камера вращается против часовой стрелки
             rotate = 1f;
         //изменение поворота камеры по координате y
-        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime * rotate, Space.World);
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime * rotate * mult, Space.World);
 
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            mult = 3f;
+        else mult = 1f;
+
+        transform.Translate(new Vector3(horizont, 0, vertical) * Time.deltaTime * mult * speed, Space.World);
     }
 
 }
