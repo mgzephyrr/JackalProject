1c7fd2bf7085bf536ededc0390fe8d7ab58339a5
Лера Белаш <vabelash@edu.hse.ru>
2023-06-24 23:51:03 +0500

    Раскидала скрипты на клетки сундка

--- Assets/Scripts/TilesScripts/Chest.cs
+++ Assets/Scripts/TilesScripts/Chest.cs
@@ -20,47 +20,39 @@ public class Chest : MonoBehaviour
         {
             if (TileBoard.gamePieces[(int)(gameObject.transform.localPosition.x - 42.5), (int)(gameObject.transform.localPosition.z - 47.5)] != null)
             {
-                pirate = TileBoard.gamePieces[(int)(gameObject.transform.localPosition.x - 42.5), (int)(gameObject.transform.localPosition.z - 47.5)].GetComponent<Pirate>();
-                if (pirate.team == board.turn % 4 && pirate.isCoin == false)
-                {
-                    Debug.Log(pirate.currentX.ToString() + " " + pirate.currentY.ToString());
-                    MoneyButton.SetActive(true);
-                }
-                else 
-                {
-                    MoneyButton.SetActive(false);
-                }
-                
-            }
-            else
-            {
-                MoneyButton.SetActive(false);
-            }
-        }
-    }
-    public void BringCoin()
-    {
-        if (numberOfMoney != 0)
-        {
-            if (TileBoard.gamePieces[(int)(gameObject.transform.localPosition.x - 42.5), (int)(gameObject.transform.localPosition.z - 47.5)] != null)
-            {
-                pirate = (Pirate)TileBoard.gamePieces[(int)(gameObject.transform.localPosition.x - 42.5), (int)(gameObject.transform.localPosition.z - 47.5)];
-                MoneyButton.SetActive(false);
+                pirate = (Pirate)TileBoard.gamePeces[(int)(gameObject.transform.localPosition.x - 42.5), (int)(gameObject.transform.localPosition.z - 47.5)]
                 pirate.isCoin = true;
-                numberOfMoney--;
-                if (numberOfMoney != 0)
+                if (!isSpawn)
                 {
-                    string name = numberOfMoney.ToString() + gameObject.name.Substring(1);
-                    var newGameObject = GameObject.Find(name);
-                    Debug.Log(name);
-                    if (newGameObject != null) newGameObject.SetActive(true);
+                    MoneyButton.SetActive(true);
+                    isSpawn = true;
                 }
-                Destroy(gameObject);
             }
         }
-        else
-        {
-            MoneyButton.SetActive(false);
-        }
     }
+    //public void BringCoin()
+    //{
+    //    if (numberOfMoney != 0)
+    //    {
+    //        if (TileBoard.gamePieces[(int)(gameObject.transform.localPosition.x - 42.5), (int)(gameObject.transform.localPosition.z - 47.5)] != null)
+    //        {
+    //            pirate = (Pirate)TileBoard.gamePieces[(int)(gameObject.transform.localPosition.x - 42.5), (int)(gameObject.transform.localPosition.z - 47.5)];
+    //            MoneyButton.SetActive(false);
+    //            pirate.isCoin = true;
+    //            numberOfMoney--;
+    //            if (numberOfMoney != 0)
+    //            {
+    //                string name = numberOfMoney.ToString() + gameObject.name.Substring(1);
+    //                var newGameObject = GameObject.Find(name);
+    //                Debug.Log(name);
+    //                if (newGameObject != null) newGameObject.SetActive(true);
+    //            }
+    //            Destroy(gameObject);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        MoneyButton.SetActive(false);
+    //    }
+    //}
 }
